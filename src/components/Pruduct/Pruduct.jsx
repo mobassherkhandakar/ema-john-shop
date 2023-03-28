de6dcb11b302fa80762c6cab097fe4a6d87d229a
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-03-28 10:37:07 +0600

    handelAddToCard added done

--- src/components/Pruduct/Pruduct.jsx
+++ src/components/Pruduct/Pruduct.jsx
@@ -3,8 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Pruduct = (props) => {
+  console.log(props);
   const { img, name, price, seller } = props.pruduct;
+  const handelAddToCard = props.handelAddToCard;
   // console.log(props.pruduct);
+
   return (
     <div className="">
       <div className="card w-full h-[508px] bg-base-100 shadow-xl">
@@ -16,7 +19,10 @@ const Pruduct = (props) => {
           <p>Price: ${price}</p>
           <p>Manufacturer: {seller}</p>
           <div className="card-actions ">
-            <button className="btn btn-primary absolute left-0 bottom-0 w-full">
+            <button
+              onClick={() => handelAddToCard(props.pruduct)}
+              className="btn btn-primary absolute left-0 bottom-0 w-full"
+            >
               Buy Now
               <FontAwesomeIcon
                 style={{ marginLeft: "5px" }}
--- src/components/Pruducts/Pruducts.jsx
+++ src/components/Pruducts/Pruducts.jsx
@@ -4,20 +4,30 @@ import "./Pruducts.css";
 
 const Pruducts = () => {
   const [pruducts, setPruducts] = useState([]);
+  const [card, setCard] = useState([]);
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setPruducts(data));
   }, []);
+  const handelAddToCard = (product) => {
+    const newCard = [...card, product];
+    setCard(newCard);
+  };
   return (
     <div className="grid grid-flow-row-dense grid-cols-4 grid-rows-4">
       <div className="pruduct-container col-span-3 grid grid-cols-1 mt-24 md:grid-cols-2 lg:grid-cols-3 gap-3">
         {pruducts.map((pruduct) => (
-          <Pruduct pruduct={pruduct} key={pruduct.id}></Pruduct>
+          <Pruduct
+            pruduct={pruduct}
+            key={pruduct.id}
+            handelAddToCard={handelAddToCard}
+          ></Pruduct>
         ))}
       </div>
       <div className="pruduct-details text-center">
         <h2 className="font-semibold text-2xl">Order Summary</h2>
+        <p>Selected Product: {card.length}</p>
       </div>
     </div>
   );
